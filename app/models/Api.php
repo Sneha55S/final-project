32bdd9d7f929ddc36d5b3d89edbb5ee196fba441
Sneha55S <114166865+Sneha55S@users.noreply.github.com>
2025-07-22 13:39:40 +0000

    minor changes

--- app/models/Api.php
+++ app/models/Api.php
@@ -17,7 +17,7 @@ class Api {
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, $query_url);
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+				// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
 
 				$json = curl_exec($ch);
 				$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -68,7 +68,7 @@ class Api {
 				curl_setopt($ch, CURLOPT_POST, true);
 				curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 				$response = curl_exec($ch);
 				$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
