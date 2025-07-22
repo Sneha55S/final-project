239a6e85897578d041386bde35c8399e3c096b6d
Sneha55S <114166865+Sneha55S@users.noreply.github.com>
2025-07-22 09:45:37 +0000

    navigation to template issue fixed

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,38 +6,38 @@ class Home extends Controller {
       $user = $this->model('User');
       $data = $user->test();
       
-      $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=".$_ENV['GEMINI'];
+   //    $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=".$_ENV['GEMINI'];
 
-      $data = array(
-          "contents" => array(
-              array(
-                  "role" => "user",
-                  "parts" => array(
-                      array(
-                          "text" => 'Please give a review of Barbie from someone that has an average of 4 out of 5.'
-                      )
-                  )
-              )
-          )
-      );
+   //    $data = array(
+   //        "contents" => array(
+   //            array(
+   //                "role" => "user",
+   //                "parts" => array(
+   //                    array(
+   //                        "text" => 'Please give a review of Barbie from someone that has an average of 4 out of 5.'
+   //                    )
+   //                )
+   //            )
+   //        )
+   //    );
 
-      $json_data = json_encode($data);
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $response = curl_exec($ch);
-      curl_close($ch);
-      if(curl_errno($ch)) {
-           echo 'Curl error: ' . curl_error($ch);
-      }
+   //    $json_data = json_encode($data);
+   //    $ch = curl_init($url);
+   //    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+   //    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+   //    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   //    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   //    $response = curl_exec($ch);
+   //    curl_close($ch);
+   //    if(curl_errno($ch)) {
+   //         echo 'Curl error: ' . curl_error($ch);
+   //    }
 
-			echo "<pre>";
-      echo $response;
-      die;
+			// echo "<pre>";
+   //    echo $response;
+   //    die;
 
-        // $this->view('home/index', ['user_data' => $data]);
+        $this->view('home/index', ['user_data' => $data]);
     }
 
 }
